eb664b2c0ecf5a62ba3dc58ab21df10de8da1ee7
Sarah SI MOUSSI <simoussi@SUPAGRO.INRA.LAN>
2018-06-22 11:29:18 +0200

    spectral_clust_files

---
+++ Python/Spectral_clustering_trophic similarity/affinity_computing.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import binarize
+
+adjw=pd.read_csv("../data/adjacence_esp.csv",sep=";",decimal=".",index_col=0)
+
+adj=pd.DataFrame(data=binarize(adjw,threshold=0.0,copy=False),columns=list(adjw.columns))
+adj["esp"]=adjw.index
+adj=adj.set_index("esp")
+
+sumcols=[adj.loc[:,x].sum() for x in adj.columns]
+sumrows=[adj.loc[x,:].sum() for x in adj.columns]
+
+### Sum_row=0 => all rows for x are set to zero => no matter transfer from x to any other specie => top predator
+toppreds=[adj.columns[i] for i in range(0,len(sumrows)) if (sumrows[i]==0 and sumcols[i]>0)]
+nopreds=[adj.columns[i] for i in range(0,len(sumrows)) if sumrows[i]==0]
+
+### Inverse logic => no matter transfer from any other specie to x => resource
+res=[adj.columns[i] for i in range(0,len(sumcols)) if (sumcols[i]==0 and sumrows[i]>0)]
+nores=[adj.columns[i] for i in range(0,len(sumcols)) if sumcols[i]==0]
+
+### Integrity test, isolated nodes removal
+isol=set(nopreds).intersection(set(nores))
+filt=adj.drop(isol,axis=1)
+filt=filt.drop(isol,axis=0)
+
+
+##### Pairwise cosine similarity
+## Prey/Resource similarity
+Ri=cosine_similarity(filt,filt)
+dfi=pd.DataFrame(data=Ri,columns=filt.columns)
+
+dfi.to_csv("../data/in_simil.csv",sep=";",decimal=".")
+
+## Predator/Consumer similarity
+Ro=cosine_similarity(filt.transpose(),filt.transpose())
+dfo=pd.DataFrame(data=Ro,columns=filt.columns)
+
+dfo.to_csv("../data/out_simil.csv",sep=";",decimal=".")
+
+df=(dfi+dfo)/2  ### Aggregating in/out trophic similarities => average trophic similarity 
+#df.to_csv("../data/simil.csv",sep=";",decimal=".")
+
+### Forcing intra-spec similarity to 1
+for i in range(0,len(df)):
+    df.iloc[i,i]=1
+
+
+df.to_csv("../data/simil_forced.csv",sep=";",decimal=".",index=True)
