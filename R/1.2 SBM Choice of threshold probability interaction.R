ad0d70c43fb4036095c3141f6eeb64dd422c69a2
Sara SI-MOUSSI <sara.simoussi@gmail.com>
2018-07-01 21:55:18 +0200

    interaction_probability_threshold_choice
    3 criteria:
      ** Preserving nodes number
      ** Preserving connexity
      ** Minimizing intragroup edges

--- R/1.2 SBM Choice of threshold probability interaction.R
+++ R/1.2 SBM Choice of threshold probability interaction.R
@@ -30,59 +30,83 @@ setwd("../data")
 # data load
 # -----------------------------------------------------------------------------
 
-SBM_adj0 <- read.csv2("adjgroups.csv", h = T, sep =";")
+SBM_adj <- read.csv2("Q_33_groups.csv", h = T, sep =";", row.names = 1)
+rownames(SBM_adj)=colnames(SBM_adj)
 # -----------------------------------------------------------------------------
-
+##### Règle de décision sur le choix de p ######
+## Criterion 1: Keep alpha ratio of nodes (trophic groups)
+th_n=1
+max_nbintra=0
+## Criterion 2: Keep graph connexity
+## Criterion 3: No intraguild interactions
 # -----------------------------------------------------------------------------
 
 #créer réseau à partir de SBM edge list & de differents seuil sur p d'interactions dans adj matrix 
 # sur le métaréseau
 
-
 # keeping interactions with p > 0.01
-p <- seq(0.005,0.995,by=0.005)
+p_range <- seq(0,0.995,by=0.001)
 
-densities<-vector(mode="integer",length=length(p))
-edges<-vector(mode="integer",length=length(p))
-nodes<-vector(mode="integer",length=length(p))
+# Following metrics are computed to keep track of changes in graph entropy as we filter edges based on weight threshold p
+densities<-vector(mode="integer",length=length(p_range)) # Graph density = nb_edges/nb_nodes
+# edges<-vector(mode="integer",length=length(p)) # Number of edges (hear interactions)
+# nodes<-vector(mode="integer",length=length(p)) # Number of connected nodes
 
-SBM_adj <- SBM_adj0[,-1]
+n=length(SBM_adj)
 
+optim_reached=FALSE
+node_preserv=TRUE
+intrag=TRUE
+connex=TRUE
 cpt=1
-for (i in p){
-  
-  SBM_adj_filt <- as.data.frame(ifelse(SBM_adj > i , 1, 0))
-  
+while (!optim_reached){
+  cpt=cpt+1
+  p=p_range[cpt]
+  ## Binarize
+  SBM_adj_filt <- as.data.frame(ifelse(SBM_adj > p , 1, 0))
+  ## Filter
   condition <- as.data.frame(rowSums(SBM_adj_filt))
   condition$colSums <- colSums(SBM_adj_filt)
   condition$cond <- rowSums(condition)
-  
   SBM_adj_filt <- SBM_adj_filt[condition$cond  > 0, condition$cond > 0]
-  
   
   # Creation du graphe
   g<-graph.adjacency(as.matrix(SBM_adj_filt))
 
+  # Re-compute metrics
+  densities[cpt-1]=graph.density(g)
+  #nodes[cpt]=vcount(g)
+  #edges[cpt]=ecount(g)
   
-  densities[cpt]=graph.density(g)
-  nodes[cpt]=vcount(g)
-  edges[cpt]=ecount(g)
+  # Node preservation criterion
+  node_preserv=(vcount(g)/n)>=th_n 
+
+  # Check for intraguild criterion
+  intra=lapply(1:length(SBM_adj),FUN=function(x) SBM_adj[x,x]>p)
+  n_intrag=length(which(intra==TRUE))
+  intrag=n_intrag>max_nbintra
   
-  cpt<-cpt+1
+  # Check for connexity criterion
+  clu<-components(g)
+  connex=clu$no==1
+  
+  if(!(node_preserv & intrag & connex)){
+    optim_reached=TRUE
+    p=p_range[cpt-1]
+  }else{
+    cpt=cpt+1
+  }
 }
 
 densities
 
 par(mfrow=c(2,2))
-plot(densities~p)
-plot(nodes~p)
-plot(edges~p)
+plot(densities~p_range)
 
-edges2 <- edges[2:length(edges)]-edges[1:(length(edges)-1)]
-plot(edges2~p[-197])
-
-##### Règle de décision sur le choix de p TODO ######
-p=0.5
+# plot(nodes~p_range)
+# plot(edges~p_range)
+# edges2 <- edges[2:length(edges)]-edges[1:(length(edges)-1)]
+# plot(edges2~p_range[-197])
 
 #########################################################
 
